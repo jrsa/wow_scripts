dbf8cea4c68057a5f6d0d2e72ce9217c3a18ee69
James Anderson <jrsa@jrsa.co>
2018-08-11 17:28:15 -0700

    look for the global wmo on disk, os.path cleanup

--- test_wdt.py
+++ test_wdt.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 
 import sys
-import os.path
+from os.path import exists, join, sep
 
 from wow.wdt import Wdt
 from wow.simple_file import load
@@ -9,11 +9,11 @@ from wow.simple_file import load
 mapname = sys.argv[1]
 
 data_root = "/Users/jrsa/wow/wowassets/wotlk/"
-maps_root = os.path.join(data_root, 'World/Maps/')
+maps_root = join(data_root, 'World/Maps/')
 wdt_filename = f"{mapname}/{mapname}.wdt"
 
 wdt = Wdt()
-wdt.read(load(os.path.join(maps_root, wdt_filename)))
+wdt.read(load(join(maps_root, wdt_filename)))
 
 print("read wdt file, version=", wdt.version)
 
@@ -21,14 +21,16 @@ if len(wdt.extant_tiles):
     print(f"found {len(wdt.extant_tiles)} tiles")
 
     for i, j in wdt.extant_tiles:
-        tile_fn = os.path.join(maps_root, f"{mapname}/{mapname}_{i}_{j}.adt")
-        if os.path.exists(tile_fn):
+        tile_fn = join(maps_root, f"{mapname}/{mapname}_{i}_{j}.adt")
+        if exists(tile_fn):
             print("adt found for", i, j)
         else:
             print("error: no adt file found for", i, j)
 
 elif len(wdt.object_filename):
-    print("found object file", wdt.object_filename)
+    wmo_path = join(data_root, sep.join(wdt.object_filename.decode('utf-8').split('\\')))
+    if exists(wmo_path):
+        print("found object file on disk: ", wmo_path)
 
 else:
     print("didnt find tiles or wmo, read a bad file without throwing an error!")
